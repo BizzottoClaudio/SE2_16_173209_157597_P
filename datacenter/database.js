58e5f3c208918a45f5ffee88ed3bf6f4bfc1af01
DeathNote93 <bestmiky@gmail.com>
2016-12-09 12:15:32 +0100

    aggiunte funzionalita' di registrazione e login utente da parte del database e del server

--- datacenter/database.js
+++ datacenter/database.js
@@ -30,6 +30,112 @@ var login_check = function (){
     return login;
 }
 
+/**
+ * @brief funzione che slogga l'utente impostando il suo status di loggato a FALSE
+ * @return un boolean indicante che l'utente è stato sloggato correttamente
+ */
+var login_exit = function (){
+    var res = false;
+    
+    if(login == true){
+        login = false;
+        res = true;
+    }else{
+        res = false
+    }
+
+    return res;
+}
+
+/**
+ * @brief Funzione che registra l'utente se l'email che utilizza non è già in uso.
+ * @param [in] name il nome dell'utente.
+ * @param [in] surname il cognome dell'utente.
+ * @param [in] email l'email dell'utente.
+ * @param [in] password la password dell'utente.
+ * @return TRUE se la registrazione è andata a buon fine, FALSE altrimenti.
+ */
+var regis = function (name, surname, email, password){
+    
+    var check = true;
+
+	//itero le email presenti nel database per verificare che non sia già presente
+	for(var i = 0; i< register.length; i++){
+	    //se l'email è già presente imposto il check a FALSE
+        if(emails[i] == email.toString()){
+	        check = false;
+        }
+	}
+	//se l'email non è presente procedo con l'inserire l'utente e le sue caratteristiche
+	if(check == true){
+	    var news = insert_user(name.toString(), surname.toString(), email.toString(), password.toString());
+        var c = [];
+        checks.push(c);
+	    register.push(news);
+	    emails.push(email.toString());
+	    passwords.push(password.toString());
+	}
+    
+    return check;
+}
+
+/**
+ * @brief Funzione che logga l'utente e torna TRUE, FALSE se il login fallisce.
+ * @param [in]  email email dell'utente.
+ * @param [in|out]  password password dell'utente.
+ * @return una variabile indicante se il login è avvenuto o no.
+ */
+var log = function (email, password){
+    var check = false;
+
+    for(var i = 0; i< register.length; i++){
+        if(emails[i] == email.toString() && passwords[i] == password.toString()){
+            check = true;
+            login = true;
+        }
+    }
+
+    return check;
+}
+
+/**
+ * @brief Funzione usata solo in fase di testing.
+ * @return ritorna la lista degli utenti registrati.
+ */
+var return_register = function(){
+    return register;
+}
+
+/**
+ * @brief Funzione che ritorna la lista dei check.
+ * @return ritorna la lista dei check.
+ */
+var return_checks = function(){
+    return checks;
+}
+
+/**
+ * @brief Funzione che ritorna la lista degli utente registrati.
+ * @return ritorna la lista delle email degli utenti registrati.
+ */
+var return_emails = function (){
+    return emails;
+}
+
+/**
+ * @brief Funzione usata solo in fase di testing.
+ * @return ritorna la lista delle passwords degli utenti registrati.
+ */
+var return_passwords = function (){
+    return passwords;
+}
 
 //exports delle funzioni
-exports.loginCheck = login_check;
+exports.loginCheck = login_check; 
+exports.registrazione = regis;
+exports.login = log; 
+exports.exit = login_exit; 
+exports.return_register = return_register; 
+exports.return_emails = return_emails; 
+exports.return_passwords = return_passwords; 
+exports.return_checks = return_checks; 
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var app = express();
 //importo i database
 var database = require('./datacenter/database.js');
 
+//variabile indicante l'email dell'utente corrente
+var email_utente = "";
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
@@ -46,6 +49,163 @@ app.post('/login_check', function(request, response){
     //imposto l'header a 200
     response.writeHead(200, headers);
     
+    response.end(json);
+});
+
+/**
+ * @brief Funzione invocata quando è richiesta la registrazione.
+ * @param [in] Object request la richiesta dell'utente.
+ * @param [out] Object response la risposta del server.
+ * @return un JSON contenente TRUE se l'utente è riuscito a registrarsi, FALSE altrimenti.
+ */
+app.post('/registrazione', function(request, response){
+    //parametri che verranno utilizzati per gestire la registrazione
+    var json;
+    var name;
+    var surname;
+    var email;
+    var password;
+    var registrazione;
+    //controllo che il body, e poi i parametri, siano stati definiti
+    if ( typeof request.body !== 'undefined' && request.body){
+        if (typeof request.body.name !== 'undefined' && request.body.name){
+            name = request.body.name;  
+        }else{ 
+            name = "not defined";
+        }
+        if (typeof request.body.surname !== 'undefined' && request.body.surname){
+            surname = request.body.surname;  
+        }else{ 
+            surname = "not defined";
+        }
+        if (typeof request.body.email !== 'undefined' && request.body.email){
+            email = request.body.email;  
+        }else{ 
+            email = "not defined";
+        }
+        if (typeof request.body.password !== 'undefined' && request.body.password){
+            password = request.body.password;  
+        }else{ 
+            password = "not defined";
+        }
+        //se i parametri sono stati definiti correttamente
+        if(name != "not defined" && surname != "not defined" && email != "not defined" && password != "not defined"){
+            //effettuo l'iserimento dell'acocunt nel database
+            registrazione = database.registrazione(name, surname, email, password);
+            //valuto l'esito dell'inserimento
+            if(registrazione == true){
+                json = JSON.stringify({ 
+                    answer: true
+                });
+            }else{
+                json = JSON.stringify({ 
+                    answer: false
+                });
+            }
+        }
+    }
+    //se il body non era stato definito correttamente
+    else{
+        name = "body undefined";
+        surname = "body undefined";
+        email = "body undefined";
+        password = "body undefined";
+    }
+    //headers a 200 se il body e i parametri sono stati correttamente definiti
+    if(name != "not defined" && surname != "not defined" && email != "not defined" && password != "not defined" && name != "body undefined" && surname != "body undefined" && email != "body undefined" && password != "body undefined"){
+        response.writeHead(200, headers);
+    }
+    //altrimenti setto l'headers a 406, per indicare problemi con almeno uno dei parametri
+    else{
+        response.writeHead(406, headers);
+    }
+
+    response.end(json);
+});
+
+/**
+ * @brief Funzione invocata quando è richiesto il login.
+ * @param [in] Object request la richiesta dell'utente.
+ * @param [out] Object response la risposta del server.
+ * @return un JSON contenente TRUE se l'utente è riuscito a loggarsi, FALSE altrimenti.
+ */
+app.post('/login', function(request, response){
+    //parametri utilizzati per gestire il login
+    var json;
+    var email;
+    var password;
+    var login;
+    //controllo che il body, e poi i parametri, siano stati definiti    
+    if ( typeof request.body !== 'undefined' && request.body){
+        if ( typeof request.body.email !== 'undefined' && request.body.email){
+            email = request.body.email;  
+        }else{ 
+            email = "not defined";
+        }
+        if ( typeof request.body.password !== 'undefined' && request.body.password){
+            password = request.body.password;  
+        }else{ 
+            password = "not defined";
+        }
+        //se i parametri sono stati definiti correttamente
+        if(email != "not defined" && password != "not defined"){
+            //effettuo il login, ritornando TRUE in caso affermativo, FALSE altrimenti
+            login = database.login(email, password);
+            if(login == true){
+                //aggiorno la variabile globale con l'email dell'utente corrente
+                email_utente = email;
+                json = JSON.stringify({ 
+                    answer: true
+                });
+            }else{
+                json = JSON.stringify({ 
+                    answer: false
+                });
+            }
+        }
+    }
+    //se il body non era stato definito correttamente
+    else{
+        email = "body undefined";
+        password = "body undefined";
+    }
+    
+    //headers a 200 se il body e i parametri sono stati correttamente definiti
+    if(email != "not defined" && password != "not defined" && email != "body undefined" && password != "body undefined"){       
+        response.writeHead(200, headers);
+    }
+    //altrimenti setto l'headers a 406, per indicare problemi con almeno uno dei parametri
+    else{
+        response.writeHead(406, headers);
+    }
+
+    response.end(json);
+});
+
+/**
+ * @brief Funzione invocata quando è richiesto il logout.
+ * @param [in] Object request la richiesta dell'utente.
+ * @param [out] Object response la risposta del server.
+ * @return un JSON contenente TRUE se l'utente è riuscito a scollegarsi, FALSE altrimenti.
+ */
+app.post('/exit', function(request, response){
+    var json;
+    //effettuo il logout, ritornando TRUE in caso affermativo, FALSE altrimenti
+    var exit = database.exit();
+    if(exit == true){
+        //aggiorno la variabile globale con un campo vuoto
+        email_utente = "";
+        json = JSON.stringify({ 
+            answer: true
+        });
+    }else{
+        json = JSON.stringify({ 
+            answer: false
+        });
+    }    
+    //imposto l'headers a 200
+    response.writeHead(200, headers);
+
     response.end(json);
 });
 
